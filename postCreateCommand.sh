8fca8b3e5674a047023f0f3b8c7f6ac5a3df23fe
pengzhendong <zhendong.peng@qq.com>
2024-08-30 15:42:17 +0800

    first commit

---
+++ postCreateCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/vscode/venv/bin/activate
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
+sudo chown vscode:vscode /workspace
+pre-commit install
