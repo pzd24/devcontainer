7f6dc247ea88e56d62be162fd7ffbae64468c760
pzd24 <pengzhendong@shoppal.ai>
2024-10-28 16:50:27 +0800

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sudo chown vscode:vscode /home/vscode/workspace
+
+export https_proxy=http://u-UE25Z3:tXGJgV92@10.255.128.104:3128
+export http_proxy=http://u-UE25Z3:tXGJgV92@10.255.128.104:3128
